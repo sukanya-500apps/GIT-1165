426cd651207cb69370a84924b498cbf6f4eaea56
sukanya-500apps <sukanya.gumparthi@500apps.com>
2022-03-24 15:09:35 +0530

    commit

---
+++ parent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class=parent-component>
+<childcomponent:message="message"/>
+    </div>
+</template>
+<script>
+import  childcomponent  from "@/components.child.vue"
+
+export default {
+       name:"parentcomponent" ,
+       data(){
+       return {
+           message:"hi sukanya",
+       };
+    },
+    components:{
+    childcomponent,
+},
+    };
+</script>
